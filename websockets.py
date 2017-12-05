9bb2eb52f42786ab74796e15840470cd2a6b0134
noamt <noam@10ne.org>
2017-12-05 16:10:21 +0200

    Initial commit

---
+++ websockets.py
@@ -0,0 +1,11 @@
+import asyncio
+import websockets
+from websockets import quotes
+
+start_server = websockets.serve(quotes.generator, '127.0.0.1', 1337)
+
+print('Webuddha is running on 127.0.0.1:1337')
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
